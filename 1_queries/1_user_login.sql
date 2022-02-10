c89734fd2ffb9d66437d0fe7c628be4e3d0fbea5
eze1er <papaezechiel@gmail.com>
2022-02-10 05:14:28 +0000

    First commit

---
+++ 1_queries/1_user_login.sql
@@ -0,0 +1,8 @@
+SELECT * 
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
+
+-- compass result
+-- SELECT id, name, email, password
+-- FROM users
+-- WHERE email = 'tristanjacobs@gmail.com';
---
+++ 1_queries/2_avg_length_reservation.sql
@@ -0,0 +1,6 @@
+SELECT AVG(end_date - start_date) AS average_duration
+FROM reservations;
+
+-- compass result
+-- SELECT avg(end_date - start_date) as average_duration
+-- FROM reservations;
