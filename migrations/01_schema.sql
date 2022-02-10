480ba2ac30b43d2bba3bb4a9d9c4d24982bbaa0c
eze1er <papaezechiel@gmail.com>
2022-02-10 20:36:47 +0000

    first commit

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -48,3 +48,11 @@ CREATE TABLE property_reviews (
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+);
