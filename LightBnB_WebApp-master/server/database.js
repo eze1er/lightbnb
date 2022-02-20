e7de06c1ed3283203bb4de58811ed308af52ef71
eze1er <papaezechiel@gmail.com>
2022-02-20 01:30:40 +0000

    all functions implemented

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -92,17 +92,6 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-//  const getAllProperties = (options, limit = 10) => {
-//   return pool
-//     .query(`
-//     SELECT * 
-//     FROM properties LIMIT $1`, [limit])
-//     .then((result) => result.rows)
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// };
-
 const getAllProperties = function(options, limit = 10) {
 
   const queryParams = [];
